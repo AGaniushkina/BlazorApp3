162173cd0a4ec2caf4e7869cd3aedcc15f91e758
AGaniushkina <annaganyushkina02@gmail.com>
2024-02-17 14:51:55 +0400

    init

---
+++ BlazorApp2/Client/Program.cs
@@ -0,0 +1,14 @@
+using BlazorApp2.Client;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Smart.Blazor;
+
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddSmart();
+
+await builder.Build().RunAsync();
