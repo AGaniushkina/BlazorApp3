2f6e093a96dbb6779e25b9777c07ae4b30371fb4
AGaniushkina <annaganyushkina02@gmail.com>
2024-02-20 19:11:24 +0400

    add passengers info

---
+++ BlazorApp2/Shared/FlightInfo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorApp2.Shared
+{
+    public class FlightInfo
+    {
+        [Required]
+        public string? DirectionFrom { get; set; } = "";
+        [Required]
+        public string? DirectionTo { get; set; } = "";
+        [Required]
+        public DateTime Departing { get; set; } = DateTime.Now;
+        public DateTime Returning { get; set; } = DateTime.Now.AddHours(24);
+        public int PassengersCount { get; set; } = 1;
+    }
+}
---
+++ BlazorApp2/Shared/PassengersInfo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorApp2.Shared
+{
+    public class PassengersInfo
+    {
+        public string? Sex { get; set; } = "";
+        [Required]
+        [StringLength(100, ErrorMessage = "String is too long.")]
+        public string? FirstName { get; set; } = "";
+        [Required]
+        [StringLength(100, ErrorMessage = "String is too long.")]
+        public string? LastName { get; set; } = "";
+        public DateTime DateOfBirth { get; set; } = DateTime.Today;
+        [Required]
+        public string? Document { get; set; } = "";
+        [Required]
+        public string? DocumentNumber { get; set; } = "";
+        public string? DocumentSeries { get; set; } = "";
+        public DateTime ExpiryDate { get; set; } = DateTime.Today;
+    }
+}
