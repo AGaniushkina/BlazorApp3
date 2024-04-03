f17e002842fe5caff5c26b033eb459e045528658
s.nikiforov <s.nikiforov@rowi.com>
2024-04-03 23:00:20 +0400

    add Flight class

---
+++ BlazorApp2/Shared/Flight.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using MongoDB.Bson;
+
+namespace BlazorApp2.Shared;
+
+public class Flight
+{
+    [BsonId]
+    [BsonRepresentation(BsonType.ObjectId)]
+    public string? Id { get; set; }
+    public string FlightNumber { get; set; } = null!;
+    public DateTime DepartureDate { get; set; }
+    public DateTime ArrivalDate { get; set; }
+    public string? Plane { get; set; }
+    public double Price { get; set; }
+    public int AvailableSeatsCount { get; set; }
+    // TODO: связать с таблицей route
+    public Route? Route { get; set; }
+}
