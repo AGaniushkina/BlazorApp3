ab7974e8d2e4083a73974c101242621974d8fd7b
simon <simonik1002@mail.ru>
2024-04-27 03:54:23 +0400

    add validation

--- BlazorApp2/Shared/AddPassenger.cs
+++ BlazorApp2/Shared/AddPassenger.cs
@@ -1,13 +1,36 @@
-﻿namespace BlazorApp2.Shared;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorApp2.Shared;
 
 public class AddPassenger
 {
-	public string LastName { get; set; } = "";
+    [Required(ErrorMessage = "Фамилия обязательна")]
+    [RegularExpression(@"^[a-zA-Z\s.\-']{2,}$", ErrorMessage = "Некорректно")]
+    public string LastName { get; set; } = "";
+
+    [Required]
+	[RegularExpression(@"^[a-zA-Z\s.\-']{2,}$", ErrorMessage = "Некорректно")]
 	public string FirstName { get; set; } = "";
+
+	[Required]
+	[RegularExpression(@"^[a-zA-Z\s.\-']{2,}$", ErrorMessage = "Некорректно")]
 	public string Patronymic { get; set; } = "";
+
+	[Required]
 	public GenderEnum Sex { get; set; }
+
+	[Required]
 	public DateTime DateOfIssue { get; set; }
+
+	[Required]
+	[RegularExpression(@"[0-9]+", ErrorMessage = "Некорректно")]
 	public string? DocumentSeriesAndNumber { get; set; } = "";
+
+	[Required]
+	[RegularExpression(@"^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$", ErrorMessage = "Некорректно")]
 	public string Email { get; set; } = "";
+
+	[Required]
+	[RegularExpression(@"[0-9]+", ErrorMessage = "Некорректно")]
 	public string PhoneNumber { get; set; } = "";
 }
